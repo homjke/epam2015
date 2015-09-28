24164ba64cc7d3b726180fbf3245fcd7ce951378
homjke <homa.php@gmail.com>
2015-09-28 23:22:48 +0300

    Test
    test commit

---
+++ SE_01/SE01_1/src/main/java/com/epam/java/khomyakov_as/epam2015/SE01_1/App.java
@@ -0,0 +1,13 @@
+package com.epam.java.khomyakov_as.epam2015.SE01_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
